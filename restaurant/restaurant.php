5762ba588de34ca82badc7c7ca7472decba4cbec
Birthday4z <43517230+Birthday4z@users.noreply.github.com>
2022-10-16 01:01:57 +0700

    add admin, customer, restaurant and rider page

---
+++ restaurant/restaurant.php
@@ -0,0 +1,28 @@
+<?php
+    $conDB = mysqli_connect("localhost", "root", "", "it-comp-65"); // เชื่อมต่อฐานข้อมูล
+    // if($conDB) {
+    //     echo "เชื่อมต่อฐานข้อมูลสำเร็จ!";
+    // }
+    // else echo "เกิดข้อผิดพลาด".mysqli_connect_error();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ร้านอาหาร - ระบบสั่งจองอาหารออนไลน์</title> <!-- เปลี่ยน Title ของเว็บเพจ (ชื่อ Tab ของเว็บ) -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet"> <!-- นำเข้า CSS ของ Bootstrap -->
+    <link href="../css/style.css" rel="stylesheet"> <!-- นำเข้า Style (ถ้ามี) -->
+</head>
+
+<body>
+
+
+    <script src="../js/jquery.min.js"></script> <!-- นำเข้า JS ของ jQuery -->
+    <script src="../js/bootstrap.bundle.min.js"></script> <!-- นำเข้า JS ของ Bootstrap -->
+</body>
+
+</html>
