0e360d96cf00b0a2551e3ff341ba284dd2fa3703
unknown <werapat.bird@gmail.com>
2022-11-01 15:35:55 +0700

    add restaurant page

---
+++ restaurant/restaurant.php
@@ -0,0 +1,32 @@
+<?php
+    require_once "../php/dbcon.php";
+    session_start();
+    
+    if(empty($_SESSION['id'])) { // ถ้ายังไม่เคย Login ให้กลับไปหน้า Login
+        header("refresh:0;url=restaurant-login.php");
+    }
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ผู้ดูแลร้านอาหาร - ระบบสั่งจองอาหารออนไลน์</title> <!-- เปลี่ยน Title ของเว็บเพจ (ชื่อ Tab ของเว็บ) -->
+    <link href="../css/bootstrap.css" rel="stylesheet"> <!-- นำเข้า CSS ของ Bootstrap -->
+</head>
+
+<body>
+    <!-- ส่วนของ Navbar -->
+    <?php require_once "navbar.php" ?>
+
+
+    <script src="../js/jquery-3.6.1.js"></script> <!-- นำเข้า JS ของ jQuery -->
+    <script src="../js/bootstrap.bundle.js"></script> <!-- นำเข้า JS ของ Bootstrap -->
+</body>
+
+</html>
